b85e5a5e0cb4d40452a594a1149c959f7f6def23
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-08-18 21:32:11 -0500

    [UPD] Complete CRUD TODO function

--- todo-serveless-csharp.Functions/Functions/TodoAPI.cs
+++ todo-serveless-csharp.Functions/Functions/TodoAPI.cs
@@ -154,5 +154,34 @@ namespace todo_serveless_csharp.Functions.Functions
                 Result = todoEntity
             });
         }
+
+        [FunctionName(nameof(DeleteTodo))]
+        public static async Task<IActionResult> DeleteTodo(
+       [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")] HttpRequest req,
+       [Table("todo", "TODO", "{id}", Connection = "AzureWebJobsStorage")] TodoEntity todoEntity,
+       [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+       string id,
+       ILogger log)
+        {
+            log.LogInformation($"Delete todo by id : {id}, received.");
+            if (todoEntity == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    isSuccess = false,
+                    Message = "Todo not found."
+                });
+            }
+            await todoTable.ExecuteAsync(TableOperation.Delete(todoEntity));
+            string message = $"Todo by id : {id}, deleted.";
+            log.LogInformation(message);
+
+            return new OkObjectResult(new Response
+            {
+                isSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
     }
 }
